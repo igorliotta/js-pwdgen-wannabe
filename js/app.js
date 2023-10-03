1d867b307e66c24b670e3aeaa341a65e06535549
igorliotta <liottaigor@gmail.com>
2023-10-03 15:03:07 +0200

    Added: draft of pseudo-code inside my app.js in the form of a comment

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// # Generatore di password!
+
+// - Chiedere all'utente il suo nome:
+//   - Dichiarare una variabile x.
+//   - Assegnare a suddetta variabile un valore, la quale dovrà essere restituita tramite la funzione prompt.
+
+// - Chiedere all'utente il suo cognome:
+//   - Dichiarare una variabile y.
+//   - Assegnare a suddetta variabile un valore, la quale anch'essa dovrà essere restituita tramite la funzione prompt.
+
+// - Chiedere all'utente il suo colore preferito:
+//    - Dichiarare una variabile z.
+//   - Assegnare a suddetta variabile un valore, la quale anch'essa a sua volta dovrà essere restituita tramite la funzione prompt.
+
+// - Concatenare i dati forniti dall'utente (nome, cognone e colore preferito):
+//   - Dichiarare una variabile password dove concateneremo le tre stringhe.
+
+// - Stampare il risultato all'interno di un h1:
+//   - Inserire nella pagina html un tag h1 che possegga un id = "password".
+//   - Recuperare tramite Js gli elementi del Dom che posseggano id = "password".
+//   - Infine modificare l'innerHTML del nostro tag h1 con il valore che abbiamo inserito nella variabile password.
