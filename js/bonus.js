986534cb8c6eeeaf6558104d40b8ac3603c0b160
igorliotta <liottaigor@gmail.com>
2023-10-03 16:13:02 +0200

    Added: function-Math-random in bonus js

--- js/bonus.js
+++ js/bonus.js
@@ -2,6 +2,11 @@
 
 // - Inserire alla fine della password finale un numero random da 1 a 99:
 //   - Dichiarare una nuova variabile j random nel quale verrà scelto in maniera randomica il numero.
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+ console.log(getRandomInt(99));
 // - Dichiarare una variabile finalPassword.
 // - Concatenare suddetta variabile j random alla variabile password iniziale.
 // - Stampare il risultato all'interno del nostro h1.
